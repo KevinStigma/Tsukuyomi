2a19b7a962015c15aa01af8cce5e5019f6a7af20
kevinstigma <781422447@qq.com>
2020-04-09 11:40:10 +0800

    change style

--- Tsukuyomi/Tsukuyomi/main.cpp
+++ Tsukuyomi/Tsukuyomi/main.cpp
@@ -1,9 +1,13 @@
 #include "Tsukuyomi.h"
 #include <QtWidgets/QApplication>
+#include <QStyleFactory>  
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	QStringList list = QStyleFactory::keys();
+	QApplication::setStyle(QStyleFactory::create("Fusion"));
+	QApplication::setPalette(QApplication::style()->standardPalette());
 	Tsukuyomi w;
 	w.show();
 	return a.exec();
