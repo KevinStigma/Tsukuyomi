df11aa25dc20cfca3cddc0eb9638d40f4d3107dc
kevinstigma <781422447@qq.com>
2020-06-08 12:03:50 +0800

    fix material bug

--- Tsukuyomi/Tsukuyomi/PbrMat/Matte.cpp
+++ Tsukuyomi/Tsukuyomi/PbrMat/Matte.cpp
@@ -23,8 +23,8 @@ void MatteMaterial::exportToXML(tinyxml2::XMLElement* elm)
 RenderLightHelper::Material MatteMaterial::generateRenderMaterial()
 {
 	RenderLightHelper::Material mat;
-	mat.Ambient = XMFLOAT4(Kd.r * 0.2, Kd.b * 0.2, Kd.g * 0.2, 1.0);
-	mat.Diffuse = XMFLOAT4(Kd.r * 0.8, Kd.b * 0.8, Kd.g * 0.8, 1.0);
+	mat.Ambient = XMFLOAT4(Kd.r * 0.2, Kd.g * 0.2, Kd.b * 0.2, 1.0);
+	mat.Diffuse = XMFLOAT4(Kd.r * 0.8, Kd.g * 0.8, Kd.b * 0.8, 1.0);
 	mat.Specular = XMFLOAT4(0.0, 0.0, 0.0, 1.0);
 	mat.Reflect = XMFLOAT4(0.0, 0.0, 0.0, 1.0);
 	return mat;
